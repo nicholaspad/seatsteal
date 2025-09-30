a3720f2036e044f1e4f0102cabe064d3bc426a39
Nicholas Padmanabhan <nicholaspad@gmail.com>
2025-09-30 00:49:27 -0700

    Setup app root

--- seatsteal/src/main.tsx
+++ seatsteal/src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { Route } from 'react-router';
+
+setupIonicReact();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <IonApp>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          <Route path="/" component={App} />
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </IonApp>
   </StrictMode>,
 )
