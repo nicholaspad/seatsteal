d576675cf407a50f98e1de1d043eae1c6a8ed7cc
Nicholas Padmanabhan <nicholaspad@gmail.com>
2025-09-30 00:52:13 -0700

    Further capacitor setup

---
+++ seatsteal/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.seatsteal.frontend',
+  appName: 'seatsteal',
+  webDir: 'build'
+};
+
+export default config;
