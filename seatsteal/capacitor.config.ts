d5dc8fbe830537f72016a2c2e3bf473f281f1950
Nicholas Padmanabhan <nicholaspad@gmail.com>
2025-10-25 10:37:58 -0700

    Setup capacitor iOS

--- seatsteal/capacitor.config.ts
+++ seatsteal/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "com.seatsteal.frontend",
   appName: "seatsteal",
-  webDir: "build",
+  webDir: "dist",
 };
 
 export default config;
