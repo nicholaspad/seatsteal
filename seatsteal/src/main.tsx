793e29ec0a40f51ad68d6155ed18450c460b9558
Nicholas Padmanabhan <nicholaspad@gmail.com>
2025-10-25 17:39:27 -0700

    Fix prod build deps, add SpeedInsights for Vercel monitoring

--- seatsteal/src/main.tsx
+++ seatsteal/src/main.tsx
@@ -5,6 +5,7 @@ import "@fontsource/inter";
 import "./index.css";
 import { IonApp, IonRouterOutlet, setupIonicReact } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import { Route, Redirect } from "react-router-dom";
 import { Toaster } from "sonner";
 
@@ -115,5 +116,6 @@ createRoot(document.getElementById("root")!).render(
         </TooltipProvider>
       </SessionProvider>
     </ThemeProvider>
+    <SpeedInsights/>
   </StrictMode>,
 );
