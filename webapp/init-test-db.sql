562bf4514ccd4bd2edd8d498cea34b2f8c0791aa
Nicholas Padmanabhan <nicholaspad@gmail.com>
2025-12-02 01:45:02 -0800

    Fix security lints: function search_path and pg_trgm extension location
    - Add migration 010 to fix Supabase security warnings:
      - Set search_path='' on 4 auth trigger functions to prevent manipulation attacks
      - Move pg_trgm extension from public to extensions schema
      - Update database search_path to include extensions schema
      - Properly revert search_path in downgrade for full reversibility
    - Update init-test-db.sql to match new extension location

--- webapp/init-test-db.sql
+++ webapp/init-test-db.sql
@@ -1,2 +1,10 @@
 -- Initialize test database with required extensions
-CREATE EXTENSION IF NOT EXISTS pg_trgm;
+-- Create extensions schema for security (extensions should not be in public)
+CREATE SCHEMA IF NOT EXISTS extensions;
+
+-- Create pg_trgm extension in extensions schema
+CREATE EXTENSION IF NOT EXISTS pg_trgm SCHEMA extensions;
+
+-- Set search_path at database level so all connections include extensions schema
+-- This is required for gin_trgm_ops operator class to be found
+ALTER DATABASE seatsteal_test SET search_path = public, extensions;
